d7eb6d9e44a269f7389960d0e8f855052e7a5cda
BhushanBadhe <89174820+BhushanBadhe@users.noreply.github.com>
2022-05-22 12:29:33 +0530

    In this section we've created custom cards using bulma.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: "Neat Tree",
+      imgUrl : 'assets/tree.jpeg',
+      username: 'nature',
+      content:  'Saw this awesome tree during my hike today.'
+    
+    },
+    {
+      title: "Snowy Mountain",
+      imgUrl : 'assets/mountain.jpeg',
+      username: 'mountain_lover',
+      content:  'Top of the world feels like heaven.'
+    
+    },
+    {
+      title: "Mountain  Biking",
+      imgUrl : 'assets/biking.jpeg',
+      username: 'rider46',
+      content:  'I did some biking today.'
+    
+    },
+    {
+      title: "Snowy Mountain",
+      imgUrl : 'assets/mountain.jpeg',
+      username: 'mountain_lover',
+      content:  'Top of the world feels like heaven.'
+    
+    }
+  ];
+
 }
